2797e63a961906ad459f7e34fd661d207efdb025
1longg <lbnlong109@gmail.com>
2024-04-04 08:00:37 +0700

    Lay danh sach nhan vien

--- server/src/app/controllers/EmployeeController.js
+++ server/src/app/controllers/EmployeeController.js
@@ -4,7 +4,35 @@ const moment = require("moment");
 
 class EmployeeController {
   // [GET] /users/employees
-  getEmployees(req, res) {}
+  getEmployees(req, res) {
+    const promise = () => {
+      return new Promise((resolve, reject) => {
+        db.query(
+          `
+            select ui.*, uai.user_name, uai.password, uai.role, uai.role from user_auth_info uai
+                inner join user_info ui
+                on uai.user_id = ui.user_id
+            where uai.role = 'emp'
+            `,
+          (err, result) => {
+            if (err) {
+              reject(err);
+            } else {
+              resolve(result);
+            }
+          }
+        );
+      });
+    };
+
+    promise()
+      .then((result) => {
+        res.status(200).send(result);
+      })
+      .catch((err) => {
+        res.status(400).send(err);
+      });
+  }
 
   // [GET] /users/employees/:user_id
   getEmployeesById(req, res) {}
