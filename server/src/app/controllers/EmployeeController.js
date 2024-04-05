e950315e5a0d623de69c0de5f7ceea3c9dd5e67b
Mylinh011003 <131513974+Mylinh011003@users.noreply.github.com>
2024-04-05 19:06:27 +0700

    commit searchEmployee

--- server/src/app/controllers/EmployeeController.js
+++ server/src/app/controllers/EmployeeController.js
@@ -38,7 +38,37 @@ class EmployeeController {
   getEmployeesById(req, res) {}
 
   // [GET] /users/employees/searching/:search_value
-  searchEmployees(req, res) {}
+  searchEmployees(req, res) {
+   const { search_value } = req.query;
+
+      const promise = () => {
+        return new Promise((resolve, reject) => {
+          db.query(
+            `
+              select ui.*, uai.user_name, uai.password, uai.role, uai.role from user_auth_info uai
+                  inner join user_info ui
+                  on uai.user_id = ui.user_id
+              where uai.role = 'emp' and ui.full_name like '%${search_value}%'
+              `,
+            (err, result) => {
+              if (err) {
+                reject(err);
+              } else {
+                resolve(result);
+              }
+            },
+          );
+        });
+      };
+
+      promise()
+        .then((result) => {
+          res.status(200).send(result);
+        })
+        .catch((err) => {
+          res.status(400).send(err);
+        });
+        }
 
   // [POST] /users/employee
   postEmployee(req, res) {}
