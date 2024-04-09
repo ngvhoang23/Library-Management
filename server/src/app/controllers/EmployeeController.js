76cd661d9313e4b111e2d328291a3990345c27da
devH <21522095@gm.uit.edu.vn>
2024-04-10 00:37:13 +0700

    SửtáTás¢n viên

--- server/src/app/controllers/EmployeeController.js
+++ server/src/app/controllers/EmployeeController.js
@@ -193,7 +193,47 @@ class EmployeeController {
   }
 
   // [PUT] /users/employee
-  editEmployee(req, res) {}
+  editEmployee(req, res) {
+    const { user_id, address, phone_num, birth_date, email_address, gender, first_name, last_name, created_at } =
+      req.body;
+
+    const user_avatar = `/user-avatars/${req?.file?.filename}`;
+
+    const updateUserInfo = () => {
+      const updateUserInfoSql = `
+        update user_info set
+        ${req?.file?.filename ? `user_avatar=${user_avatar ? `'${user_avatar}'` : null},` : ""} 
+        phone_num=${phone_num ? `'${phone_num}'` : null}, 
+        address=${address ? `'${address}'` : null}, 
+        birth_date=${birth_date ? `'${birth_date}'` : null},
+        email_address=${email_address ? `'${email_address}'` : null}, 
+        gender=${gender ? `${gender}` : null}, 
+        first_name=${first_name ? `'${first_name}'` : null}, 
+        last_name=${last_name ? `'${last_name}'` : null},
+        full_name='${first_name ? first_name : ""} ${last_name ? last_name : ""}',
+        created_at=${created_at ? `'${created_at}'` : null}
+        where user_id=${user_id}
+      `;
+      return new Promise((resolve, reject) => {
+        db.query(updateUserInfoSql, (err, result) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(result);
+          }
+        });
+      });
+    };
+
+    updateUserInfo()
+      .then((result) => {
+        res.status(200).send(result);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(400).send(err);
+      });
+  }
 }
 
 module.exports = new EmployeeController();
