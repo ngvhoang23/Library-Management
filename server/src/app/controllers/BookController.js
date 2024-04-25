799966fd2f9137a29fb7e067519e720c99369339
1longg <lbnlong109@gmail.com>
2024-04-25 07:11:43 +0700

    lay danh sach cac nhom sach va sach

--- server/src/app/controllers/BookController.js
+++ server/src/app/controllers/BookController.js
@@ -4,7 +4,35 @@ const moment = require("moment");
 
 class BookController {
   // [GET] /books
-  getBooks(req, res) {}
+  getBooks(req, res) {
+    const promise = () => {
+      return new Promise((resolve, reject) => {
+        db.query(
+          `
+          select * from books 
+          inner join book_detail on books.book_detail_id = book_detail.book_detail_id
+          inner join authors on book_detail.author_id = authors.author_id
+          inner join categories on book_detail.category_id = categories.category_id
+        `,
+          (err, result) => {
+            if (err) {
+              reject(err);
+            } else {
+              resolve(result);
+            }
+          }
+        );
+      });
+    };
+
+    promise()
+      .then((result) => {
+        res.status(200).send(result);
+      })
+      .catch((err) => {
+        res.status(400).send(err);
+      });
+  }
 
   // [GET] /books/:book_detail_id
   getBooksByGroup(req, res) {}
