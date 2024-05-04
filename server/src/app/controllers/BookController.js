589130748fffefe9379f2837b9f9dcbb39a0a5f2
devH <21522095@gm.uit.edu.vn>
2024-05-04 20:56:44 +0700

    TáLấy nhóm sách bằng Id & Sửa nhóm sách

--- server/src/app/controllers/BookController.js
+++ server/src/app/controllers/BookController.js
@@ -126,7 +126,54 @@ class BookController {
   }
 
   // [GET] /books/book-groups/:book_detail_id
-  getBookGroup(req, res) {}
+  getBookGroup(req, res) {
+    const { user_id, role } = req.userInfo;
+
+    const { book_detail_id } = req.query;
+
+    console.log(role);
+
+    let sql = "";
+
+    if (role == "emp" || role == "admin") {
+      sql = `
+      select * from book_detail
+        inner join authors on book_detail.author_id = authors.author_id
+        inner join categories on book_detail.category_id = categories.category_id
+        where book_detail_id=${book_detail_id}
+      `;
+    }
+
+    if (role === "reader") {
+      sql = `
+      select *, getAvailableBookQuantity(book_detail.book_detail_id) as available, getTotalOfBookDetail(book_detail.book_detail_id) as total_book, getReadsOfBookDetail(book_detail.book_detail_id) as _reads from book_detail
+        inner join authors on book_detail.author_id = authors.author_id
+        inner join categories on book_detail.category_id = categories.category_id
+        where book_detail.book_detail_id=${book_detail_id}
+      `;
+    }
+
+    const promise = () => {
+      return new Promise((resolve, reject) => {
+        db.query(sql, (err, result) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(result);
+          }
+        });
+      });
+    };
+
+    promise()
+      .then((result) => {
+        res.status(200).send(result);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(400).send(err);
+      });
+  }
 
   // [GET] /books/books-groups/searching/:search_value
   searchBookGroups(req, res) {}
@@ -153,7 +200,87 @@ class BookController {
   deleteBook(req, res) {}
 
   // [PUT] /books/book-groups
-  editBookGroup(req, res) {}
+  editBookGroup(req, res) {
+    const { book_detail_id, book_name, price, published_date, author_id, description, category_id, publish_com } =
+      req.body;
+
+    let cover_photo = null;
+
+    if (req?.file?.filename) {
+      cover_photo = `/book-cover-photos/${req?.file.filename}`;
+    }
+
+    let updatedBook = [
+      book_name || null,
+      price || null,
+      published_date || null,
+      author_id || null,
+      description || null,
+      category_id || null,
+      publish_com || null,
+      book_detail_id,
+    ];
+
+    if (cover_photo) {
+      updatedBook = [
+        book_name || null,
+        price || null,
+        published_date || null,
+        author_id || null,
+        description || null,
+        category_id || null,
+        publish_com || null,
+        cover_photo,
+        book_detail_id,
+      ];
+    }
+
+    const updateBook = () => {
+      let updateUserInfoSql = `
+            update book_detail set 
+            book_name=?,
+            price=?, 
+            published_date=?, 
+            author_id=?, 
+            description=?,
+            category_id=?, 
+            publish_com=?
+            where book_detail_id=?
+          `;
+      if (cover_photo) {
+        updateUserInfoSql = `
+            update book_detail set 
+            book_name=?,
+            price=?, 
+            published_date=?, 
+            author_id=?, 
+            description=?,
+            category_id=?, 
+            publish_com=?,
+            cover_photo=?
+            where book_detail_id=?
+          `;
+      }
+      return new Promise((resolve, reject) => {
+        db.query(updateUserInfoSql, updatedBook, (err, result) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(result);
+          }
+        });
+      });
+    };
+
+    updateBook()
+      .then((result) => {
+        res.status(200).send(result);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(400).send(err);
+      });
+  }
 
   // [PUT] /books/
   editBook(req, res) {}
